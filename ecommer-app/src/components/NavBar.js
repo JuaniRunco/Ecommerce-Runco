32e2c4fc591ecd97dab81d985cfddb293bedb3b8
Juani Runco <juan.runcocarreno@fce.uncu.edu.ar>
2022-07-16 15:14:29 -0300

    componente navbar

---
+++ ecommer-app/src/components/NavBar.js
@@ -0,0 +1,41 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+
+function CollapsibleExample() {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">MarkTech</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#features">Catálogo</Nav.Link>
+                        <Nav.Link href="#pricing">Contacto</Nav.Link>
+                        <NavDropdown title="Categorías" id="collasible-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">TELEVISORES</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">
+                                NOTEBOOKS
+                            </NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.">PC ESCRITORIO</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">PC GAMER</NavDropdown.Item>
+                            <NavDropdown.Divider />
+                            <NavDropdown.Item href="#action/3.4">
+                                VER MÁS
+                            </NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                    <Nav>
+                        <Nav.Link href="#deets">Ingresar</Nav.Link>
+                        <Nav.Link eventKey={2} href="#memes">
+                            Carrito
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default CollapsibleExample;
