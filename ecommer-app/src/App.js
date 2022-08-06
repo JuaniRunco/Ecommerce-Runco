ea18bca37a3d2297d28bcbcdcc863ba87bba61de
Juani Runco <juan.runcocarreno@fce.uncu.edu.ar>
2022-08-06 14:03:58 -0300

    Desafio: Sincronizar counter

--- ecommer-app/src/App.js
+++ ecommer-app/src/App.js
@@ -4,6 +4,7 @@ import NavBar from './components/NavBar';
 import ItemListContainer from './components/ItemListContainer'
 import ItemDetailContainer from './components/ItemDetailContainer';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Cart from './components/Cart';
 
 function App() {
 
@@ -16,6 +17,8 @@ function App() {
             <Route path='/' element={<ItemListContainer greeting="Bienvenido a MarkTech" />} />
             <Route path='/category/:categoryId' element={<ItemListContainer greeting="Bienvenido a MarkTech" />} />
             <Route path='/item/:id' element={<ItemDetailContainer />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path='*' element={<ItemListContainer greeting="Bienvenido a MarkTech" />} />
           </Routes>
         </div>
       </BrowserRouter>
