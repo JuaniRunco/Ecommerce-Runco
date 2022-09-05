74e654e94a479cb16e6ee3a681712b77ee633256
Juani Runco <juan.runcocarreno@fce.uncu.edu.ar>
2022-09-05 18:23:02 -0300

    Ultimos detalles del proyecto

--- ecommer-app/src/components/ItemDetailContainer.js
+++ ecommer-app/src/components/ItemDetailContainer.js
@@ -8,20 +8,30 @@ const ItemDetailContainer = () => {
 
     const { id } = useParams();
     const [product, setProduct] = useState();
+    const [loading, setLoading] = useState(true);
 
     const obtainProduct = async () => {
 
         const found = await getItem(id);
         setProduct(found);
+        setLoading(false);
     }
 
     useEffect(() => {
         obtainProduct();
+        // eslint-disable-next-line
     }, []);
 
     return (
-        <div style={{ background: 'rgb(242, 243, 244)', height: '100vh' }}>
-            {product ? <ItemDetail item={product} /> : <Loading />}
+        <div style={{ background: 'rgb(242, 243, 244)', height: '100vh', textAlign: 'center' }}>
+            {loading && <Loading />}
+            {product ?
+                <ItemDetail item={product} />
+                :
+                <h1 style={{ paddingTop: '20px' }}>
+                    404 NOT FOUND
+                </h1>
+            }
         </div>
     );
 }
